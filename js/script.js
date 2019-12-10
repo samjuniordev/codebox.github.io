4c1d5f80e203dc0bbc998bbc7617af31e835148b
samjuniordev <58047704+samjuniordev@users.noreply.github.com>
2019-12-10 22:43:06 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+	setTimeout(function(){
+		document.getElementById("my-time").style.display="block";
+	},1000);
+
+
+	setTimeout(function(){
+		document.getElementById("my-cric").style.display="none";
+	},1000);
+
+
+
